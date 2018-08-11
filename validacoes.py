bc270194e42632a2c5c3dae8edf2e126464499cc
Tyson Filardi <leonardofilardi@gmail.com>
2018-08-11 08:40:38 -0300

    alterações

--- validacoes.py
+++ validacoes.py
@@ -1,11 +1,28 @@
 from eve_sqlalchemy.validation import ValidatorSQL
+from datetime import datetime
 from flask import abort
 # from erros import PtBrHandler
 
 
 class VinculoValidator(ValidatorSQL):
 
+    def validar_maioridade(dt_nasc):
+        now = (datetime.now())
+        print(now.year)
+        print(now.month)
+        print(now.day)
+        print(type(dt_nasc))
+
+
+
+
     def validar_infomacoes(json_items):
-        print(json_items)
+        VinculoValidator.validar_maioridade(json_items[0]['dt_nasc'])
+        print(json_items[0]['cpf'])
+        print(json_items[0]['nome'])
+        print()
+
+
+
 
 
